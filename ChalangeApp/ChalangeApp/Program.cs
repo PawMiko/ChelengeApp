558a9e45cd76eee2c51571323170b3a9fcd4bae0
mikos.pawel <mikos.pawel@o2.pl>
2023-03-13 22:28:46 +0100

    poprawiony dzien 4

--- ChalangeApp/ChalangeApp/Program.cs
+++ ChalangeApp/ChalangeApp/Program.cs
@@ -1,24 +1,18 @@
 ﻿using System.ComponentModel.Design;
 
 string name = "Ewa";
-bool sex = true;
+bool IsWoman = true;
 int age = 33;
 
 
-if (sex)
+if (IsWoman)
 {
     if (age < 30)
         Console.WriteLine("kobieta ponieżej 30 lat");
-    else if ( age==33 && name=="Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
+
+    else if (age == 33 && name == "Ewa")
+            Console.WriteLine("Ewa, lat 33");
 }
-else if (!sex )
-{
-    Console.WriteLine("niepełnoletni Mężczyzna");
-}
-
-
-
+else if (!IsWoman)
+        Console.WriteLine("niepełnoletni Mężczyzna");
 
