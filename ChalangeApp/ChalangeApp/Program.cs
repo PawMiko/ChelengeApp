2308ff8d1ead24c38b7a500aeae212ad3da66d84
mikos.pawel <mikos.pawel@o2.pl>
2023-03-13 21:22:48 +0100

    dzien 4

--- ChalangeApp/ChalangeApp/Program.cs
+++ ChalangeApp/ChalangeApp/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("dzin dobrywieczór");
+﻿using System.ComponentModel.Design;
+
+string name = "Ewa";
+bool sex = true;
+int age = 33;
+
+
+if (sex)
+{
+    if (age < 30)
+        Console.WriteLine("kobieta ponieżej 30 lat");
+    else if ( age==33 && name=="Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (!sex )
+{
+    Console.WriteLine("niepełnoletni Mężczyzna");
+}
+
+
+
+
