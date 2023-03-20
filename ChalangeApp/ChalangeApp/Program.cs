76b50b98cd06febd9e018b12f4484f1a9458b34c
mikos.pawel <mikos.pawel@o2.pl>
2023-03-20 15:58:52 +0100

    zadanie dnia 6. program działa ale postanowiłem dodać opcję  w której to  pracownicy sa wyswietlanie jeżeli maja taka sama ilość punktów. niestety nie wpadłem jak bardziej elegancko to napisac. mam poczucie że można to zrobic lepiej.

--- ChalangeApp/ChalangeApp/Program.cs
+++ ChalangeApp/ChalangeApp/Program.cs
@@ -1,36 +1,111 @@
 ﻿
-using System.Xml.Schema;
+using ChalangeApp;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 
-int number = 98765432;
-Console.WriteLine("liczba do odwrocenia to "+ number);
-string numberInString2=number.ToString();
-char[] digit=numberInString2.ToCharArray();
-char temp;
-int meter = 0;
-
-
-List<char> digitList=new List<char>();
+Employee eployee1 = new Employee(" Pawel"," Muc",33);
+Employee eployee2 = new Employee(" Olek", " Kwiat", 43);
+Employee eployee3 = new Employee(" Tomek", " Wit", 53);
 
 
-for (var i = 0; i < digit.Length; i++)
-{
-  digitList.Add(digit[i]);
+
+
+Console.WriteLine("NAME " + eployee1.Name);
+Console.WriteLine("LASTNAME "+ eployee1.Lastname);
+Console.WriteLine("AGE " + eployee1.age);
+eployee1.ADDPOINT(1);
+eployee1.ADDPOINT(1);
+eployee1.ADDPOINT(1);
+eployee1.ADDPOINT(14);
+eployee1.ADDPOINT(1);
+Console.WriteLine("POINT " + eployee1.Result + "\n");
+
+Console.WriteLine("NAME "+ eployee2.Name);
+Console.WriteLine("LASTNAME " + eployee2.Lastname);
+Console.WriteLine("AGE " + eployee2.age);
+eployee2.ADDPOINT(1);
+eployee2.ADDPOINT(1);
+eployee2.ADDPOINT(14);
+eployee2.ADDPOINT(1);
+eployee2.ADDPOINT(1);
+Console.WriteLine("POINT " + eployee2.Result + "\n");
+
+Console.WriteLine("NAME " + eployee3.Name);
+Console.WriteLine("LASTNAME " + eployee3.Lastname);
+Console.WriteLine("AGE " + eployee3.age);
+eployee3.ADDPOINT(14);
+eployee3.ADDPOINT(1);
+eployee3.ADDPOINT(1);
+eployee3.ADDPOINT(1);
+eployee3.ADDPOINT(1);
+Console.WriteLine("POINT " + eployee3.Result + "\n");
+
+
  
-  meter++;
-}
-
-for (var i = 0; i < digitList.Count; i++)
+List<Employee> emploers = new List<Employee>()
 {
-    meter--;
-   
-    if(meter>i)
+    eployee1,eployee2,eployee3
+};
+int maxResult = -1;
+
+
+Employee userWithMaxResult=null;
+
+foreach (var emp in emploers)
+{
+      if (emp.Result >= maxResult)
     {
-        temp = digitList[i];
-        digitList[i] = digitList[meter];
-        digitList[meter] = temp;
+        userWithMaxResult = emp;
+        maxResult = userWithMaxResult.Result;
+             
     }
     
 }
-var result =string.Join("", digitList);
-Console.WriteLine("liczba po odwroceniu "+ result);
+
+
+if ((eployee1.Result == eployee2.Result) && (eployee1.Result == eployee3.Result) && (eployee2.Result == eployee2.Result))
+{
+    Console.WriteLine("\t" + " najwiecej punktow mają wszyscy ");
+}
+else
+if (eployee1.Result == maxResult )
+{
+
+    Console.WriteLine("\t" + " najwiecej punktow mają ");
+    Console.WriteLine(eployee1.Name); ;
+    Console.WriteLine(eployee1.Lastname);
+    Console.WriteLine(eployee1.age);
+    Console.WriteLine(eployee1.Result);
+    if (eployee1 != userWithMaxResult)
+    {
+        Console.WriteLine(userWithMaxResult.Name);
+        Console.WriteLine(userWithMaxResult.Lastname);
+        Console.WriteLine(userWithMaxResult.age);
+        Console.WriteLine(userWithMaxResult.Result);
+    }
+}
+else if (eployee2.Result == eployee3.Result)
+{
+    Console.WriteLine("\t" + " najwiecej punktow mają ");
+    Console.WriteLine(eployee2.Name); ;
+    Console.WriteLine(eployee2.Lastname);
+    Console.WriteLine(eployee2.age);
+    Console.WriteLine(eployee2.Result);
+
+    Console.WriteLine("\t" + " I równierz ");
+    Console.WriteLine(eployee3.Name); ;
+    Console.WriteLine(eployee3.Lastname);
+    Console.WriteLine(eployee3.age);
+    Console.WriteLine(eployee3.Result);
+
+}else
+{
+    Console.WriteLine("\t" + " najwiecej punktow ma ");
+    Console.WriteLine(userWithMaxResult.Name);
+    Console.WriteLine(userWithMaxResult.Lastname);
+    Console.WriteLine(userWithMaxResult.age);
+    Console.WriteLine(userWithMaxResult.Result);
+}
+
+
 
---
+++ ChalangeApp/ChalangeApp/User.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace ChalangeApp
+{
+   public class Employee
+    {
+
+        private List<int> point = new List<int>();
+
+        public Employee(string name, string lastname, int age)
+        {
+
+            this.Name = name;
+            this.Lastname = lastname;
+            this.age = age;
+
+        }
+
+        public string Name {  get;  private set; }
+        public string Lastname { get; private set; }
+        public int age { get; private set; }
+       
+        public int Result
+        {
+            get
+            {
+                return this.point.Sum();
+            }
+        }
+
+        public void ADDPOINT(int x)
+        {
+            this.point.Add(x);
+
+        }
+      
+
+
+    }
+}
