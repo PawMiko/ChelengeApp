242a653d60510d6f332f55f6e25e4c3f91be3b8f
mikos.pawel <mikos.pawel@o2.pl>
2023-03-26 20:36:37 +0200

    zadanie 7  dodanie dodatkowego testu

---
+++ ChalangeApp/ChalangeApp.Tests/EmployerTests.cs
@@ -0,0 +1,49 @@
+namespace ChalangeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void When_User_Collect_Plus_OR_Minus_Point_should_Return_Correct_Result()
+        {
+            //arrange
+            var user = new Employee("pawel", "d23r", 32);
+            user.ADDPOINT(10);
+            user.ADDPOINT(1);
+            user.REMOVEPOINT(1);
+
+            //act
+
+            var result = user.Result;
+
+            //assert
+            Assert.AreEqual(10, result);
+
+
+
+            //  Assert.Pass();
+        }
+
+
+        [Test]
+        public void When_User_Collect_Plus_OR_Minus_Point_should_Return_Correct_Result_1()
+        {
+            //arrange
+            var user = new Employee("pawel", "d23r", 32);
+            user.ADDPOINT(10);
+            user.ADDPOINT(1);
+            user.REMOVEPOINT(1);
+
+            //act
+
+            var result = user.Result;
+
+            //assert
+            Assert.AreEqual(11, result);
+
+
+
+            //  Assert.Pass();
+        }
+
+    }
+}
